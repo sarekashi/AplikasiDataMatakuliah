52d99245e039dd1fd454b4ace649dc35c1f0b51d
sarekashi <sarekashi@tuta.io>
2022-02-25 20:05:38 +0700

    fix: return comment type for List Matakuliah2

--- src/datamatakuliah/DBMatakuliah.java
+++ src/datamatakuliah/DBMatakuliah.java
@@ -31,7 +31,7 @@ public class DBMatakuliah {
     /**
      * Method ini digunakan untuk menyusun query ambil data dari DB.
      * 
-     * @return Ini mengembalikan daftar Objek Matakuliah {@link List<Matakuliah#Matakuliah(String, String, int, String)>}
+     * @return Ini mengembalikan daftar Objek Matakuliah {@link List}
      * @throws SQLException
      */
     public List<Matakuliah> list() throws SQLException {
@@ -44,7 +44,7 @@ public class DBMatakuliah {
      * secara spesifik.
      * 
      * @param keyword
-     * @return Ini mengembalikan daftar Objek Matakuliah {@link List<Matakuliah#Matakuliah(String, String, int, String)>}
+     * @return Ini mengembalikan daftar Objek Matakuliah {@link List}
      * @throws SQLException
      */
     public List<Matakuliah> list(String keyword) throws SQLException {
